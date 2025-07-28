ac029f436d54780becda0da64f07f0309607bc2a
Braj12345678 <kaushik.vaghasia@gmail.com>
2025-07-28 18:59:13 +0800

    Add files via upload

---
+++ lesson17.4python.py
@@ -0,0 +1,10 @@
+#Make a function for addition and subtaction  
+def add(x, y):
+    '''This function adds 2 numbers'''
+    return x + y 
+def subtract(x, y):
+    '''This function subtracts 2 numbers'''
+    return x - y
+# Example
+print(f"Addition of 3 and 4: {add(3, 4)}")
+print(f"subtraction of 5 and 2: {subtract(5, 2)}")
